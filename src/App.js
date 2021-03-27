19a1694cf3b921d8f8b121a131137edc2e9312ec
Swarnab Garang <swarnabgarang@gmail.com>
2021-03-27 23:30:03 +0530

    added Notes and Create components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import './App.css';
+import Create from './components/Create/Create';
+import Notes from './components/Notes/Notes';
 
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" >
+          <Route exact path="/">
+            <Notes />
           </Route>
           <Route path="/create">
+            <Create />
           </Route>
         </Switch>
       </Router>
---
+++ src/components/Notes/Notes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notes = () => {
+    return (
+        <div className="Notes">
+            <h1>Notes Page</h1>
+        </div>
+    )
+}
+
+export default Notes
