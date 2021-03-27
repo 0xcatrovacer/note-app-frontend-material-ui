898119573d0097446e77f15df36d6384eea73cb6
Swarnab Garang <swarnabgarang@gmail.com>
2021-03-28 01:57:09 +0530

    styled the Create component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import './App.css';
 import Create from './components/Create/Create';
 import Notes from './components/Notes/Notes';
 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#4db5ff',
+      dark: '#0095ff',
+      light: '#63beff'
+    }
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }
+})
+
 const App = () => {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Notes />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Notes />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,9 +1,120 @@
 import React from 'react'
 
+import './Create.css'
+
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
+import Container from "@material-ui/core/Container"
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+import { makeStyles } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
+
+const useStyles = makeStyles({
+    field: {
+        marginTop: 20,
+        marginBottom: 20,
+        display: 'block',
+        "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+            borderColor: "white"
+        },
+        "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+            borderColor: "lightgray"
+        },
+        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#4db5ff"
+        },
+        "& .MuiOutlinedInput-input": {
+            color: "white"
+        },
+        "&:hover .MuiOutlinedInput-input": {
+            color: "lightgray"
+        },
+        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
+            color: "#4db5ff"
+        },
+        "& .MuiInputLabel-outlined": {
+            color: "white"
+        },
+        "&:hover .MuiInputLabel-outlined": {
+            color: "lightgray"
+        },
+        "& .MuiInputLabel-outlined.Mui-focused": {
+            color: "#4db5ff"
+        }
+    },
+    title: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 40,
+        marginBottom: 100,
+        fontSize: 30
+    },
+    button: {
+        width: 200,
+        marginTop: 40,
+        height: 50,
+        paddingRight: 0,
+        fontWeight: 700
+    }
+})
+
 const Create = () => {
+
+    const classes = useStyles()
+
     return (
         <div className="Create">
-            <h1>Create Page</h1>
+            <Container>
+                <Typography
+                    variant="h5"
+                    component="h2"
+                    color="primary"
+                    className={classes.title}
+                    gutterBottom
+                >
+                    Create a New Note
+            </Typography>
+
+                <form noValidate autoComplete='off' className='CreateNoteForm'>
+                    <TextField
+                        label='Note Title'
+                        variant='outlined'
+                        color='primary'
+                        fullWidth
+                        required
+                        className={classes.field}
+                        InputProps={{
+                            className: "CreateInput"
+                        }}
+                        InputLabelProps={{
+                            className: "CreateInputLabel"
+                        }}
+                    />
+                    <TextField
+                        label='Note Content'
+                        variant='outlined'
+                        color='primary'
+                        multiline
+                        rows={6}
+                        fullWidth
+                        required
+                        className={classes.field}
+                        InputLabelProps={{
+                            className: "CreateInputLabel"
+                        }}
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                        endIcon={<KeyboardArrowRightIcon />}
+                    >
+                        Submit
+                </Button>
+                </form>
+            </Container>
         </div>
     )
 }
