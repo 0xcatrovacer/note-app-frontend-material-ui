2fc0336d851d15afd12efde190378b14129efec4
Swarnab Garang <swarnabgarang@gmail.com>
2021-04-03 15:34:15 +0530

    added axios rewiring to Notes content

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
+import DeleteIcon from '@material-ui/icons/Delete';
+import axios from 'axios';
 
 const Notes = () => {
+
+    const [notes, setNotes] = useState(null);
+
+    const callFn = () => {
+        const token = localStorage.getItem('token')
+
+        axios.get(`${process.env.REACT_APP_NOTERAPP_BACKEND}/notes`, {
+            headers: { 'Authorization': `Bearer ${token}` },
+        }).then((res) => {
+            setNotes(res.data)
+        })
+            .catch((err) => {
+                console.log(err.message)
+            })
+    }
+
+    useEffect(() => {
+
+        callFn()
+    }, [])
+
+
     return (
         <div className="Notes">
             <Grid container>
-                <Grid item xs={12} sm={6} md={3}>
-                    <Paper>a</Paper>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3}>
-                    <Paper>b</Paper>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3}>
-                    <Paper>c</Paper>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3}>
-                    <Paper>d</Paper>
-                </Grid>
+                {notes && notes.map((note) => (
+                    <Grid item xs={12} sm={6} md={3}>
+                        <Paper>
+                            <h2>{note.title}</h2>
+                            <div>{note.content}</div>
+                        </Paper>
+                    </Grid>
+                ))}
             </Grid>
         </div>
     )
